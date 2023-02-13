aeb55f67173cad0305229be37b8f4f1dcda2174d
Tuong Q. Phung <tuongphung@Tuongs-MacBook-Pro.local>
2023-02-14 10:23:52 +1100

    add fzflash & fs and those key blinding

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,5 +21,30 @@ alias ls "ls -p -G -al"
 alias la "ls -A"
 alias lla "ll -A"
 
+function fzflash
+  set home_dir "/Users/tuongphung"
+  set file_name "$argv[1]"
+
+  set selected_path (find $home_dir -mindepth 0 \( -path "$home_dir'/\'" -o -fstype sysfs -o -fstype devfs -o -fstype devtmpfs \) -prune \
+    -o \( -type f -print -o -type d -print -o -type l -print \) 2> /dev/null | fzf)
+
+  if [ -n "$selected_path" ]
+    if test -d "$selected_path"
+      set -gx FZF_TMP_DIR "$selected_path"
+      cd $FZF_TMP_DIR;
+    else
+      set -gx FZF_TMP_DIR (dirname $selected_path)
+      cd $FZF_TMP_DIR;
+      echo $selected_path
+    end
+  end
+end
+
+function fs -d "Switch tmux session"
+  tmux list-sessions -F "#{session_name}" | fzf | read -l result; and tmux switch-client -t "$result"
+end
+
+
 # Start Up the Starship theme
+# zoxide init fish | source
 starship init fish | source
